2c493a0f3482ee90dba858c010396827c17bc824
Dennis <dennis.kinyanjui@student.moringaschool.com>
2024-03-24 10:32:03 +0300

    complete lab~

---
+++ index.js
@@ -0,0 +1,30 @@
+function receivesAFunction(callback) {
+
+    callback();
+  }
+
+  
+  function returnsANamedFunction() {
+    function namedFunction() {
+
+      console.log("This is a named function.");
+    }
+    return namedFunction;
+  }
+
+
+  
+  function returnsAnAnonymousFunction() {
+
+
+    return function() {
+      console.log("This is an anonymous function.");
+
+    };
+  }
+  
+  receivesAFunction(function() {
+    
+    console.log("Callback function called from receivesAFunction.");
+  });
+ 
